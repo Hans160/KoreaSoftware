5ed5d1bc32b58236985180ddc0f8365152555f91
Eun-Tae Han <cyclone160@naver.com>
2026-04-29 09:32:30 +0900

    중간 수정 및 추가

--- 6.JS/1.DOM/js/buttons.js
+++ 6.JS/1.DOM/js/buttons.js
@@ -1,22 +1,12 @@
+// 각자의 버튼이 눌릴때마다 위에 div의 값을 가져다가 +1 또는 -1 을 한다.
+const button1 = document.getElementById('incButton');
+const button2 = document.getElementById('decButton');
 
-        //각자의 버튼이 눌릴때마다 위에 div의 숫자가 1씩 증가하거나 감소하도록 만들어보자
-        const zero = document.getElementById('zero');
+/* 이벤트 핸들러 */
+button1.addEventListener('click', () => {
+    document.getElementById('result').textContent = parseInt(result.textContent) + 1;
+});
 
-        function plus() {
-            zero.textContent = parseInt(zero.textContent) + 1;
-        }
-
-        function minus() {
-            zero.textContent = parseInt(zero.textContent) - 1;
-        }
-
-        const button1 = document.getElementById('incButton');
-        const button2 = document.getElementById('decButton');
-
-        /* 이벤트 핸들러 */
-        button1.addEventListener('click',()=> {
-            zero.textContent = parseInt(zero.textContent) + 1;
-        });
-        button2.addEventListener('click',()=> {
-            zero.textContent = parseInt(zero.textContent) - 1;
-        });
+button2.addEventListener('click', () => {
+    document.getElementById('result').textContent -= 1;
+});
