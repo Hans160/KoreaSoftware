3855d516a6a8f9105d563134534d65727423c5d8
Eun-Tae Han <cyclone160@naver.com>
2026-05-11 17:18:35 +0900

    requests 수정

--- 7.PYTHON/2.module/4.request.py
+++ 7.PYTHON/2.module/4.request.py
@@ -2,6 +2,9 @@
 # 그러면, pypi.org로 부터 다운로드 받아서 나의 "가상환경"에 설치가 됨
 import requests
 
-# 외부에 HTTP 요청을 대신 해주는 라이브러리
-resp = requests.get("https://www.example.com")
+# # 외부에 HTTP 요청을 대신 해주는 라이브러리
+# resp = requests.get("https://www.example.com")
+# print(resp.text)
+
+resp = requests.get('https://api.github.com')
 print(resp.text)
